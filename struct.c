6a72db3ed947ec2fb2bbe4acb218d0c2c4aa3975
r888800009 <r888800009@gmail.com>
2022-03-18 11:46:27 +0800

    Add struct for ghidra reverse engineering

---
+++ struct.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+  struct student {
+    char name[20];
+    int age;
+    char id[20];
+  };
+
+  struct student s1;
+  printf("Enter name: ");
+  scanf("%s", s1.name);
+  printf("Enter age: ");
+  scanf("%d", &s1.age);
+  printf("Enter id: ");
+  scanf("%s", s1.id);
+
+  printf("Name: %s\n", s1.name);
+  printf("Age: %d\n", s1.age);
+  printf("Id: %s\n", s1.id);
+
+  return 0;
+}
