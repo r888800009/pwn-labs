33322d6ef6ac134c4a559bc17d1de08b41a7a507
r888800009 <r888800009@gmail.com>
2022-03-18 11:55:09 +0800

    Update struct using pointer

--- struct.c
+++ struct.c
@@ -9,17 +9,19 @@ int main(int argc, char *argv[]) {
     char id[20];
   };
 
-  struct student s1;
-  printf("Enter name: ");
-  scanf("%s", s1.name);
-  printf("Enter age: ");
-  scanf("%d", &s1.age);
-  printf("Enter id: ");
-  scanf("%s", s1.id);
+  struct student *s1;
+  s1 = (struct student *)malloc(sizeof(struct student));
 
-  printf("Name: %s\n", s1.name);
-  printf("Age: %d\n", s1.age);
-  printf("Id: %s\n", s1.id);
+  printf("Enter name: ");
+  scanf("%s", s1->name);
+  printf("Enter age: ");
+  scanf("%d", &s1->age);
+  printf("Enter id: ");
+  scanf("%s", s1->id);
+
+  printf("Name: %s\n", s1->name);
+  printf("Age: %d\n", s1->age);
+  printf("Id: %s\n", s1->id);
 
   return 0;
 }
