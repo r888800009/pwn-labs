cd7da8da3b24aab759e64a768b2106ddbe58c48c
r888800009 <r888800009@gmail.com>
2022-05-27 22:23:47 +0800

    Add got_cpp

---
+++ dynamic-linking/got_cpp.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+ 解 AIS3 Pre Exam 2022 utf8 editor (maple3142)
+ 修改 C++ GOT 我們可以直接改兩個 cout 會用到 got
+ 改一個 gets 一個 system 就可以 getshell
+ */
+
+// g++ -o got_cpp got_cpp.cpp -z lazy -no-pie
+int main() {
+  for (int i = 0; i < 10; i++) cout << "Hello World" << endl;
+  return 0;
+}
+
